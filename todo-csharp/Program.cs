fb75be1257cb6334f55f35a846e5299cb7d0e57e
racoonpapa <racoonpapa@naver.com>
2023-09-17 15:02:26 +0900

    fix: blog posting

---
+++ todo-csharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using todo_csharp;
+
+var stopWatch = new Stopwatch();
+var jsonData = File.ReadAllText("todo.json");
+
+stopWatch.Start();
+var list = TodoList.FromJson(jsonData);
+stopWatch.Stop();
+
+var elapsedFrom = stopWatch.ElapsedMilliseconds * 0.001;
+
+if (list != null)
+{
+    // list.Print();
+    
+    stopWatch.Restart();
+    list.ToJson();
+    stopWatch.Stop();
+
+    var elapsedTo = stopWatch.ElapsedMilliseconds * 0.001;
+    
+    Console.WriteLine($"fromJson - {elapsedFrom} seconds, toJson - {elapsedTo} seconds");
+}
---
+++ todo-csharp/Todo.cs
@@ -0,0 +1,92 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace todo_csharp;
+
+public enum Priority
+{
+    None = 0,
+    Low = 1,
+    Medium = 2,
+    High =3
+}
+
+public class TodoItem
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+    [JsonPropertyName("content")]
+    public string Content { get; set; }
+    [JsonPropertyName("due_date")]
+    public DateTime? DueDate { get; set; }  = null;
+    [JsonPropertyName("priority")]
+    public Priority Priority { get; set; }
+    [JsonPropertyName("done")]
+    public bool Done { get; set; }
+
+    public TodoItem(string content)
+    {
+        Id = Guid.NewGuid().ToString();
+        Content = content;
+    }
+
+    public TodoItem WithDueDate(DateTime? dueDate)
+    {
+        DueDate = dueDate;
+        return this;
+    }
+
+    public TodoItem WithPriority(Priority priority)
+    {
+        Priority = priority;
+        return this;
+    }
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder($"[{(Done ? 'x' : ' ')}] {Content}");
+
+        if (Priority != Priority.None) builder.Append($" [{Priority}]");
+        if (DueDate != null) builder.Append($" ({DueDate?.ToString("yyyy-MM-dd")})");
+        
+        return builder.ToString();
+    }
+}
+
+public class TodoList
+{
+    private Dictionary<string, TodoItem> _items = new Dictionary<string, TodoItem>();
+
+    // .NET Core 3.0+, .NET 5/6 or higher required 
+    public static TodoList? FromJson(string json)
+    {
+        if (string.IsNullOrEmpty(json))
+            return null;
+
+        try
+        {
+            var items = JsonSerializer.Deserialize<TodoItem[]>(json);
+            if (items == null) return null;
+
+            var newList = new TodoList();
+            foreach (var item in items) newList._items.Add(item.Id, item);
+
+            return newList;
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    public string ToJson()
+    {
+        return JsonSerializer.Serialize(_items.Values);
+    }
+
+    public void Print()
+    {
+        foreach(var item in _items) Console.WriteLine(item.Value.ToString());
+    }
+}
